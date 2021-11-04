ebdc8e43fb2443964cc90eba822247f9882e35bd
Aman <aksinha.101010@gmail.com>
2021-11-04 15:23:22 +0530

    add google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pexel_wall/pages/explore_page.dart';
 import 'package:pexel_wall/provider/image_provider.dart';
 import 'package:pexel_wall/provider/internet_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
   runApp(const App());
 }
 
@@ -14,24 +21,32 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (_) => InternetProvider(),
-            lazy: true,
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => InternetProvider(),
+          lazy: true,
+        ),
+        ChangeNotifierProvider(
+          create: (_) => CuratedImageProvider(),
+          lazy: true,
+        ),
+        ChangeNotifierProvider(
+          create: (_) => LikedImageProvider(),
+          lazy: true,
+        ),
+      ],
+      child: MaterialApp(
+        theme: ThemeData.light()
+          ..textTheme.apply(
+            fontFamily: GoogleFonts.varelaRound().fontFamily,
           ),
-          ChangeNotifierProvider(
-            create: (_) => CuratedImageProvider(),
-            lazy: true,
+        darkTheme: ThemeData.dark()
+          ..textTheme.apply(
+            fontFamily: GoogleFonts.varelaRound().fontFamily,
           ),
-          ChangeNotifierProvider(
-            create: (_) => LikedImageProvider(),
-            lazy: true,
-          ),
-        ],
-      child: const MaterialApp(
         themeMode: ThemeMode.system,
         debugShowCheckedModeBanner: false,
-        home: ExplorePage(),
+        home: const ExplorePage(),
       ),
     );
   }
